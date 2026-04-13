cfe3b0833e03603a6d53859dca6928af6bcba151
Cassiano Godoy <cassianoknopf@gmail.com>
2026-04-13 11:46:43 +0100

    corrige app streamlit render

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-dados = pd.read_csv('C:/Users/cassi/projeto/vehicles.csv')
+dados = pd.read_csv('vehicles.csv')
 
-st.header('Dashboard de anúncios de carros')
-st.write('Permite visualizar dados de anúncios')
+st.header("Dashboard de anúncios de carros")
 
-hist_button = st.button('Cria Histograma')
+st.write(dados.head())
+
+hist_button = st.button("Criar histograma")
 
 if hist_button:
-    st.write('Criando histograma de Kms dos veículos')
-    fig = px.histogram(dados, x='odometer')
-    st.plotly_chart(fig, use_container_width=True)
+    st.write("Histograma da quilometragem")
+    fig = px.histogram(dados, x="odometer")
+    st.plotly_chart(fig)
 
-scatter_button = st.button('Criar gráfico de dispersão')
+scatter_button = st.button("Criar gráfico de dispersão")
 
 if scatter_button:
-    st.write('Criando gráfico de dispersão entre km e preço')
-    fig = px.scatter(dados, x='odometer', y='price')
-    st.plotly_chart(fig, use_container_width=True)
+    st.write("Relação entre KM e preço")
+    fig = px.scatter(dados, x="odometer", y="price")
+    st.plotly_chart(fig)
